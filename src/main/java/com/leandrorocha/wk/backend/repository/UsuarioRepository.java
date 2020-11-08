b5adc6d7f066efab9bebe65eabea2320b7384e48
leandrorochaadm <leandrorochaadm@gmail.com>
2020-11-08 05:44:00 -0400

    resource adicionando usuario

--- src/main/java/com/leandrorocha/wk/backend/repository/UsuarioRepository.java
+++ src/main/java/com/leandrorocha/wk/backend/repository/UsuarioRepository.java
@@ -1,17 +1,15 @@
 package com.leandrorocha.wk.backend.repository;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import com.leandrorocha.wk.backend.model.Usuario;
 
 public class UsuarioRepository {
-	List<Usuario> lista = new ArrayList<Usuario>();
-
-	public UsuarioRepository() {
-		lista.add(new Usuario(1L, "Leandro", "leandro@gmail.com", "asdf"));
-		lista.add(new Usuario(2L, "Jose", "jose@gmail.com", "asdf"));
-	}
+	static List<Usuario> lista = new ArrayList<Usuario>(
+			Arrays.asList(new Usuario(1L, "Leandro", "leandro@gmail.com", "asdf"),
+					new Usuario(2L, "Jose", "jose@gmail.com", "asdf")));
 
 	public List<Usuario> listarTodos() {
 		return lista;
@@ -19,5 +17,10 @@ public class UsuarioRepository {
 
 	public Usuario buscarPorId(Long id) {
 		return lista.get(id.intValue() - 1);
+	}
+
+	public Usuario adiciona(Usuario usuario) {
+		usuario.setId(new Long(lista.size() + 1));
+		return lista.add(usuario) ? usuario : null;
 	}
 }
--- src/main/java/com/leandrorocha/wk/backend/resource/UsuarioResource.java
+++ src/main/java/com/leandrorocha/wk/backend/resource/UsuarioResource.java
@@ -1,15 +1,19 @@
 package com.leandrorocha.wk.backend.resource;
 
+import java.net.URI;
 import java.util.List;
 
 import com.leandrorocha.wk.backend.model.Usuario;
 import com.leandrorocha.wk.backend.repository.UsuarioRepository;
 
+import jakarta.ws.rs.Consumes;
 import jakarta.ws.rs.GET;
+import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
 
 @Path("usuarios")
 public class UsuarioResource {
@@ -31,4 +35,16 @@ public class UsuarioResource {
 		return repository.buscarPorId(id);
 	}
 
+	@POST
+	@Consumes(MediaType.APPLICATION_JSON)
+	public Response adiciona(Usuario usuario) {
+
+		Usuario usuarioCriado = repository.adiciona(usuario);
+
+		URI uri = URI.create("/wk.backend/webapi/usuarios/" + usuario.getId());
+		if (usuarioCriado == null)
+			return Response.serverError().build();
+		return Response.created(uri).build();
+
+	}
 }
